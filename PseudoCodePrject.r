27f915f2a03fd33987fc258eb4a068e58f77892d
EdgDew <edgarddewitte@gmail.com>
2016-11-11 16:14:33 +0100

    a
    a

---
+++ PseudoCodePrject.r
@@ -0,0 +1,31 @@
+
+mydir <- "/Programming/ef/"
+data_Foot <- read.csv(paste0(mydir, "Ligue1data.csv"))
+
+
+n = 20
+P_toul_16 = data []
+P_psg_16 = data []
+Qual = c (1:n)
+
+Qual(i)=  points toul 16 * (points Psg - data(emplacement points 16 i )/points psg)
+
+---
+
+MaxPD = c ()
+PDD = c ()
+PDA = c ()
+Beta = c ()
+
+MaxPD (i) = (187) - (20-i)
+for i in (1:n)
+    for j in (1:n)
+        PDA (i) = PDA (i) + (20 - dat (,j))
+    
+    
+PDD (i) = Max PD (i) - PDA (i)
+
+Beta (i) = PDD (i) / PDA (i)
+
+Forme (i) = data (emplacement points i 17) x Beta (i) + data (emplacement points i 17)
+    
